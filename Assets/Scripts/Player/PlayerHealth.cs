4673e9e8bc44492e5d348655e56398715a56fb98
Joni V <joni.voutilainen@edu.karelia.fi>
2026-02-03 12:05:39 +0200

    Added goblin enemy and scripts for player health and damage handling

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
