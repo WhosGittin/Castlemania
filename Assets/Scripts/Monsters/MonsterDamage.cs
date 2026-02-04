da3c146aadea9b0d9bea72a36e54f9e90d88df53
Joni V <joni.voutilainen@edu.karelia.fi>
2026-02-04 15:04:44 +0200

    Added a knockback effect to goblin when he hits the player. Also hid the bat from the scene, as its bugging out

--- Assets/Scripts/Monsters/MonsterDamage.cs
+++ Assets/Scripts/Monsters/MonsterDamage.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 
 public class MonsterDamage : MonoBehaviour
 {
-    public int damage;
-    public PlayerHealth playerHealth;
+	public int damage;
+	/*Allow access to other scripts */
+	public PlayerHealth playerHealth;
+	public MonsterMovement monsterMovement;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            playerHealth.TakeDamage(damage);
-        }
-    }
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			playerHealth.TakeDamage(damage);
+			monsterMovement.KBCounter = monsterMovement.KBTotalTime;
+			if (collision.transform.position.x <= transform.position.x)
+			{
+				monsterMovement.KnockFromRight = true;
+			}
+			else
+			{
+				monsterMovement.KnockFromRight = false;
+			}
+		}
+	}
 }
--- Assets/Scripts/Monsters/MonsterMovement.cs
+++ Assets/Scripts/Monsters/MonsterMovement.cs
@@ -10,6 +10,18 @@ public class MonsterMovement : MonoBehaviour
 	//For keeping track of the player position
 	public Transform playerTransform;
 
+	/*Knockback related variables for when monster hits the player*/
+	//The force of the knockback
+	public float KBForce;
+	//Timer to track knockback duration
+	public float KBCounter;
+	//Total duration of the knockback
+	public float KBTotalTime;
+	//Direction of the knockback
+	public bool KnockFromRight;
+
+	public Rigidbody2D monsterRb;
+
 	public void Start()
 	{
 		//Find the player in the scene by searching for the GameObject with the "Player" tag
@@ -35,7 +47,33 @@ public class MonsterMovement : MonoBehaviour
 			transform.localScale = new Vector3(-monsterScale, monsterScale, 1);
 		}
 
-		//Move towards the player
-		transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+	}
+
+	//Physics-related things can be handled here if needed (ex. Movement). Everything else in Update()
+	void FixedUpdate()
+	{
+		//Stop movement when the monster is knocked back
+		if (KBCounter <= 0)
+		{
+			//Move towards the player
+			transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+		}
+		else
+		{
+			//Check direction of the knockback
+			if (KnockFromRight)
+			{
+				//Apply knockback force to the right and upwards
+				monsterRb.linearVelocity = new Vector2(KBForce, KBForce);
+			}
+			else
+			{
+				//Apply knockback force to the left and upwards
+				monsterRb.linearVelocity = new Vector2(-KBForce, KBForce);
+			}
+			//Decrease the knockback timer
+			KBCounter -= Time.deltaTime;
+		}
+
 	}
 }
