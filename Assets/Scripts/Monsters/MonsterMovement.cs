f158f962a37b201c3dc9a056feb5e0fed47facb0
Joni V <joni.voutilainen@edu.karelia.fi>
2026-02-04 13:55:32 +0200

    Added monsterScale variable to keep monster size from resetting when its turning

--- Assets/Scripts/Monsters/MonsterMovement.cs
+++ Assets/Scripts/Monsters/MonsterMovement.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class MonsterMovement : MonoBehaviour
 {
+	//Monster movement speed
 	public float moveSpeed;
+	//Scale of the monster sprite
+	public float monsterScale;
 
 	//For keeping track of the player position
 	public Transform playerTransform;
+
+	public void Start()
+	{
+		//Find the player in the scene by searching for the GameObject with the "Player" tag
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		if (player != null)
+		{
+			playerTransform = player.transform;
+		}
+	}
 
 	// Update is called once per frame
 	void Update()
@@ -14,12 +27,12 @@ public class MonsterMovement : MonoBehaviour
 		if (transform.position.x < playerTransform.position.x)
 		{
 			//Turn the sprite to face right
-			transform.localScale = new Vector3(1, 1, 1);
+			transform.localScale = new Vector3(monsterScale, monsterScale, 1);
 		}
 		else
 		{
 			//Turn the sprite to face left
-			transform.localScale = new Vector3(-1, 1, 1);
+			transform.localScale = new Vector3(-monsterScale, monsterScale, 1);
 		}
 
 		//Move towards the player
