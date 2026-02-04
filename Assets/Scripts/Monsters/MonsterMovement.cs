75ed8e0f2fb5ef77ebebd19d08d0e54f7754fa8d
Joni V <joni.voutilainen@edu.karelia.fi>
2026-02-04 12:01:40 +0200

    Testing knockback for monster hits. Not working

--- Assets/Scripts/Monsters/MonsterMovement.cs
+++ Assets/Scripts/Monsters/MonsterMovement.cs
@@ -2,27 +2,27 @@ using UnityEngine;
 
 public class MonsterMovement : MonoBehaviour
 {
-    public float moveSpeed;
+	public float moveSpeed;
 
-    //For keeping track of the player position
-    public Transform playerTransform;
+	//For keeping track of the player position
+	public Transform playerTransform;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //If the player is to the right of the monster
-        if (transform.position.x < playerTransform.position.x)
-        {
-            //Turn the sprite to face right
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else
-        {
-            //Turn the sprite to face left
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
+	// Update is called once per frame
+	void Update()
+	{
+		//If the player is to the right of the monster
+		if (transform.position.x < playerTransform.position.x)
+		{
+			//Turn the sprite to face right
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+		else
+		{
+			//Turn the sprite to face left
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
 
-        //Move towards the player
-        transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
-    }
+		//Move towards the player
+		transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+	}
 }
